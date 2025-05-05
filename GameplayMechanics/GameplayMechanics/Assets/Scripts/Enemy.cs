a92b96b2824e23076c3bdb88272be2d29373896e
marquezzr <marquezr@roverkids.org>
2025-05-05 14:05:45 -0400

    completed 4.4

--- GameplayMechanics/GameplayMechanics/Assets/Scripts/Enemy.cs
+++ GameplayMechanics/GameplayMechanics/Assets/Scripts/Enemy.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
@@ -18,5 +20,9 @@ public class Enemy : MonoBehaviour
     {
         Vector3 lookDirection = (player.transform.position - transform.position).normalized;
         enemyRb.AddForce(lookDirection * speed);
+        if(transform.position.y < -10)
+        {
+            Destroy(gameObject);
+        }
     }
 }
